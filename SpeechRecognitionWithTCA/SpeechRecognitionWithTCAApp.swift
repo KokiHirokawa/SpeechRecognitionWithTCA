16d9c423a2845608cb64bc16de82a5f6dc31dba8
KokiHirokawa <kitakitsune1110@gmail.com>
2022-01-07 22:01:35 +0900

    Initial Commit

---
+++ SpeechRecognitionWithTCA/SpeechRecognitionWithTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechRecognitionWithTCAApp.swift
+//  SpeechRecognitionWithTCA
+//
+//  Created by 廣川昂紀 on 2022/01/07.
+//
+
+import SwiftUI
+
+@main
+struct SpeechRecognitionWithTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
